dd1712b493a71d3045ccc2098b6064fe93ba5ba9
Johannes Sluis <joesluis51@gmail.com>
2026-02-14 17:02:03 -0800

    add basic auth and refresh list UI

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { basicAuth } from 'hono/basic-auth'
 import { z } from 'zod'
 
 type Env = {
   DB: D1Database
+  ADMIN_USER: string
+  ADMIN_PASSWORD: string
 }
 
 const app = new Hono<{ Bindings: Env }>()
@@ -13,7 +16,7 @@ app.use(
   cors({
     origin: ['http://localhost:4321', 'https://joe-list.vercel.app'],
     allowMethods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
-    allowHeaders: ['Content-Type'],
+    allowHeaders: ['Content-Type', 'Authorization'],
   })
 )
 
@@ -25,11 +28,21 @@ const CreateLinkSchema = z.object({
   title: z.string()
 })
 
+const CreateLoginSchema = z.object({
+  password: z.string()
+})
+
+const adminAuth = basicAuth({
+  verifyUser: (username, password, c) => {
+    return username === c.env.ADMIN_USER && password === c.env.ADMIN_PASSWORD
+  },
+})
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 });
 
-app.post('/link', async (c) => {
+app.post('/link', adminAuth, async (c) => {
   const raw = await c.req.json()
   const parsed = CreateLinkSchema.safeParse(raw);
   if (!parsed.success) {
@@ -46,7 +59,7 @@ app.post('/link', async (c) => {
   return c.text("created", 201)
 });
 
-app.delete('/link/:id', async (c) => {
+app.delete('/link/:id', adminAuth, async (c) => {
   const id = c.req.param('id');
 
   const result = await c.env.DB
@@ -64,6 +77,21 @@ app.get('/links', async (c) => {
 
   return c.json(result.results)
 });
+
+app.post('/login', async (c) => {
+  const raw = await c.req.json();
+  const parsed = CreateLoginSchema.safeParse(raw);
+  if (!parsed.success) {
+    return c.json({ error: parsed.error }, 400);
+  }
+
+  console.log(c.env.ADMIN_PASSWORD);
+  if (parsed.data.password !== c.env.ADMIN_PASSWORD) {
+    return c.text("??", 401);
+  }
+
+  return c.text("good job", 200);
+})
 
 app.get('/*', (c) => {
   return c.text('Invalid endpoint', 404)
